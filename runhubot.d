7b0df29385dcb789a8b00cdf53362f66f768f2c0
arakawa <arakawa@bbt757.com>
2015-01-22 12:19:48 +0900

    delete

--- runhubot.d
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# runhubot
-
-export HUBOT_IRC_NICK="hubot"
-export HUBOT_IRC_ROOMS="#botch_hubot"
-export HUBOT_IRC_SERVER="irc.freenode.net"
-
-/Users/arakawa/myhubot/bin/hubot -a irc
